4b1def29c18901e483f7550a1b0083bfedcb96ff
Chirag Singh Toor <chiragtoor@gmail.com>
2018-11-20 22:58:07 -0800

    cleanup unused commands, update README

--- lib/cellular.ex
+++ lib/cellular.ex
@@ -1,8 +1,8 @@
 defmodule Cellular do
-def connect do
+  def connect do
+    # must create the /dev/ppp device
     System.cmd("mknod", ["/dev/ppp", "c", "108", "0"])
-    System.cmd("modprobe", ["option"])
-    File.write!("/sys/bus/usb-serial/drivers/option1/new_id", "05c6 90b2")
+    # use pppd to call our nova chatscript with /dev/ttyUSB2
     System.cmd("/usr/sbin/pppd", ["call", "nova", "/dev/ttyUSB2"])
   end
 end
